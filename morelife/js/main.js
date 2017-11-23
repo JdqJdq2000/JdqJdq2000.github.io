764a35965cf6ceecbe81791134776a88ea547eae
JdqJdq2000 <32189574+JdqJdq2000@users.noreply.github.com>
2017-11-23 15:57:03 -0500

    Final project v1.8
    Created collapse image boxes using Jquery.

--- morelife/js/main.js
+++ morelife/js/main.js
@@ -1,1 +1,17 @@
-// Make it rain!
+$('#checkBox1').on('click', function() {
+  $('#centerpic').fadeToggle(300);
+});
+
+$('#checkBox2').on('click', function() {
+  $('#aboutuspic').fadeToggle(300);
+});
+
+$('#checkBox3').on('click', function() {
+  $('#prefooterpic').fadeToggle(300);
+});
+
+// $("#check1").hover(function(){
+//     $(this).css("transform", "scale(2)");
+//     }, function(){
+//     $(this).css("transform", "scale(1)");
+// });

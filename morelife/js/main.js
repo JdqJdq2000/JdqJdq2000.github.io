66955b6aada0702f5ce1340a8240d7929b79dd89
JdqJdq2000 <32189574+JdqJdq2000@users.noreply.github.com>
2017-11-25 01:58:19 -0500

    Final Project v1.9
    Product animations using JQuery, some fine tuning of responsiveness continues...smooth scrolling
    next, I need to tackle burger menu and pop up windows...

--- morelife/js/main.js
+++ morelife/js/main.js
@@ -1,17 +1,79 @@
 $('#checkBox1').on('click', function() {
-  $('#centerpic').fadeToggle(300);
+  $('#centerpic').fadeToggle(400);
 });
 
 $('#checkBox2').on('click', function() {
-  $('#aboutuspic').fadeToggle(300);
+  $('#aboutuspic').fadeToggle(400);
 });
 
 $('#checkBox3').on('click', function() {
-  $('#prefooterpic').fadeToggle(300);
+  $('#prefooterpic').fadeToggle(400);
+  $('footer').css('margin-top', '20px');
+  if (!$("#checkBox3").is(":checked")) {
+    $('footer').css('margin-top', '0');
+  }
 });
 
-// $("#check1").hover(function(){
-//     $(this).css("transform", "scale(2)");
-//     }, function(){
-//     $(this).css("transform", "scale(1)");
-// });
+$('a[href*="#"]').on('click', function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    console.log('[name=' + this.hash.slice(1) + ']');
+    var name1 = this.hash.slice(1);
+    // $('#wellcapsules').addClass('rotatewellcapsules');
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      switch (this.hash.slice(1)) {
+        case "restspray":
+        case "relaxspray":
+        case "focusspray":
+        case "welldrops":
+        case "healcream":
+          $('html, body').animate({
+            scrollTop: target.offset().top - 180
+          }, 800);
+          break;
+        default:
+          $('html, body').animate({
+            scrollTop: target.offset().top - 90
+          }, 800);
+      };
+    }
+  }
+});
+
+
+$('#restanchor').on('click', function() {
+
+  setTimeout(function() {
+    $('#restspray').toggleClass('rotateproduct');
+  }, 500);
+});
+
+$('#relaxanchor').on('click', function() {
+  setTimeout(function() {
+    $('#relaxspray').toggleClass('rotateproduct');
+  }, 500);
+});
+
+$('#focusanchor').on('click', function() {
+  setTimeout(function() {
+    $('#focusspray').toggleClass('rotateproduct');
+  }, 500);
+});
+
+$('#wellanchor').on('click', function() {
+  $('#welldrops').toggleClass('rotateproduct');
+  $('#wellcapsules').toggleClass('rotateproduct');
+});
+
+$('#healanchor').on('click', function() {
+  $('#healcream').toggleClass('rotateproduct');
+});
